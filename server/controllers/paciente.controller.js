084d0bb425afd8c0fb29fa2c903e5c235c015c04
menelik <menelix.mesa@nauta.cu>
2020-11-22 23:01:16 -0500

    Se realizaron los metodos de obtener paciente dado un id, acutalizar un paciente asi como eliminar un paciente dado un id

--- server/controllers/paciente.controller.js
+++ server/controllers/paciente.controller.js
@@ -5,30 +5,56 @@ const pacienteCtrl = {};
 pacienteCtrl.getPacientes = async (req , res ) => {
 
    const todosPacientes = await Pacient.find();
- // todosPacientes = {"menelik" : "montano"}
-
-   res.json(todosPacientes);
-
-           
+    res.json(todosPacientes);
+          
 };
 
-pacienteCtrl.savePaciente= async (req, res) => {
-    const pasc = new ModelPacient(req.body);
+pacienteCtrl.createPacient= async (req, res) => {
+    const pasc = new Pacient(req.body);
     await pasc.save();
     
     res.json({
-        'status' : 'Guardado'
+        'status' : 'Pacient saved'
     });
    
 
 };
 
 pacienteCtrl.getPacient= async (req, res ) => {
-    const paciente = await ModelPacient.findById(req.params.id)
+    //console.log(req.params);
+    const getpacient = await Pacient.findById(req.params.id);
+    res.json(getpacient);
 };
 
-pacienteCtrl.updatePacient= function(){};
+pacienteCtrl.updatePacient= async(req,res)=>{
+    const {id} =req.body;
+    const updpacient ={    
+           
+                name : req.body.body,
+                lastName : req.body.lastName,
+                person_Id : req.body.person_Id,
+                genre : req.body.genre,
+                address :req.body.address,
+                tel : req.body.tel,
+                        
+             work: {  
+                     name_work:req.body.name_work ,
+                     tel_work:req.body.tel_work,
+                 address_work :req.body.address_work
+               }	
+           	
 
-pacienteCtrl.deletePacient= function(){};
+    }
+    await Pacient.findByIdAndUpdate(id,{$set: updpacient},{new : true});
+    res.json({'status' : 'Pacient Updated'});
+
+};
+
+pacienteCtrl.deletePacient= async (req,res) => {
+  await Pacient.findByIdAndRemove(req.params.id);
+res.json({'status':'Pacient delete'});
+
+
+};
 
 module.exports = pacienteCtrl;
